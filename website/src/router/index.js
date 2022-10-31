e212d25f4d395e01de261f0e1372a4c33f58e335
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-10-31 13:42:48 +0100

    Add the about location to the router

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,20 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: function () {
+      return import('../views/HomeView.vue')
+    }
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      return import('../views/AboutView.vue')
+    }
+  },
+  {
+    path: '/locations',
+    name: 'locations',
+    component: function () {
+      return import('../views/LocationsView.vue')
     }
   }
 ]
