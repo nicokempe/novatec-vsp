3a2a80045e292a569628aa17bfe8b307f1ac039b
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-10-31 08:43:16 +0000

    Install Vue i18n

--- website/src/main.js
+++ website/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueI18n from 'vue-i18n';
 import './assets/style/app.css';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(VueI18n).mount('#app');
