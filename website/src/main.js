36eac3cc1f6dbfed27207542dc505b5e6c9b2839
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-10-31 10:16:45 +0100

    Uninstall i18n v8

--- website/src/main.js
+++ website/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import VueI18n from 'vue-i18n';
 import './assets/style/app.css';
 
-createApp(App).use(router).use(VueI18n).mount('#app');
+createApp(App).use(router).mount('#app');
