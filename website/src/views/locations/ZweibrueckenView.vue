be30cd47f3602700e87ccffe71d0fc9f76307a48
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-11-02 16:46:58 +0100

    Add the location views
    * Berlin
    * Frankfurt
    * Granada
    * Hamburg
    * Hanover
    * Karlsruhe
    * Leinfelden-Echterdingen
    * Munich
    * Zweibrücken

---
+++ website/src/views/locations/ZweibrueckenView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-white">
+      <div class="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 class="text-2xl font-bold tracking-tight text-gray-900">Novatec Consulting GmbH</h2>
+        <h3 class="text-2xl tracking-tight text-gray-600"> --- Zweibrücken</h3>
+      </div>
+    </div>
+  </template>
