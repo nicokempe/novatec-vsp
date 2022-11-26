d485e23326819c4214f81510ca1689c33ae8dd05
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-11-26 23:01:47 +0100

    Add the site views

---
+++ website/src/views/locations/sites/LeinfeldenEchterdingenView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl py-16 px-12 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+      <LocationHead :siteName="siteNameData" :siteLocation='siteLocationData' :siteCreated='siteCreatedData' :applicationFilter='applicationFilterData'></LocationHead>
+      <div class="mx-auto max-w-2xl py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <video autoplay muted loop>
+          <source :src="video" type="video/mp4">
+        </video>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LocationHead from '@/components/LocationHead';
+
+export default {
+components: {
+  LocationHead
+},
+data() {
+  return {
+    siteNameData: 'Leinfelden-Echterdingen',
+    siteLocationData: 'Baden-Württemberg, Deutschland',
+    siteCreatedData: '2020',
+    applicationFilterData: '/career/search?filter=leinfelden-echterdingen',
+    video: require('@/assets/videos/novatec-le-presentation.mp4')
+  }
+}
+}
+</script>
