cd302d36a222dbd2ae6f7f04953dcd1766f8e8a4
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-11-26 22:29:36 +0100

    Update the App
    * Add the static header
    * Add the static footer
    * Update the font
      * Add Poppins
      * Add Roboto

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,30 +1,30 @@
+<!-- 
+  Make the static components optional
+-->
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header/>
   <router-view/>
+  <Footer/>
 </template>
 
+<script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&family=Roboto&display=swap');
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: 'Poppins', sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 </style>
