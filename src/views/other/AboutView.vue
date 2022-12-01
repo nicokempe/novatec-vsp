384ed6bf09a09bed6e8105169af694145534d26b
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-01 01:32:25 +0100

    Add the about view

---
+++ src/views/other/AboutView.vue
@@ -0,0 +1,23 @@
+<!--
+  TODO: Add skeleton loading animation
+-->
+<template>
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl sm:px-6 lg:max-w-7xl lg:px-8"> <!-- removed: py-16 sm:py-24 -->
+      <Team></Team>
+      <References></References>
+    </div>
+  </div>
+</template>
+
+<script>
+import Team from '@/components/Team.vue';
+import References from '@/components/References.vue';
+
+ export default {
+  components: {
+    Team,
+    References
+  }
+ }
+</script>
