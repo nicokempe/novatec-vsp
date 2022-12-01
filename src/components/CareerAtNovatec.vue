cce696a11f1748cd81d590572ef13602789a48c1
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-01 01:31:08 +0100

    Add default components
    * Career at Novatec
    * References
    * Team

---
+++ src/components/CareerAtNovatec.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="relative bg-black">
+    <div class="h-56 bg-cyan-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+    <img class="h-full w-full object-cover" src="https://www.novatec-gmbh.de/wp-content/uploads/Augmented-und-Virtual-Reality_Mini-Bild-Detailseite-Faktenbox_Novatec_HR_Remote-Meeting_0054_20210729-e1664391797629.jpg" alt="" />
+    </div>
+    <div class="relative mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+    <div class="md:ml-auto md:w-1/2 md:pl-10">
+        <p class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Career at Novatec</p>
+        <p class="mt-3 text-lg text-gray-300">You like what you have read so far? They are exactly your topics? Find out about our open positions and how much fun it is to work for us on our careers page.</p>
+        <div class="mt-8">
+        <div class="inline-flex rounded-md shadow">
+            <RouterLink to="/career" class="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-gray-900 hover:bg-gray-50">
+            Become part of the team
+            <i class="fa-solid fa-paper-plane -mr-1 ml-3 h-5 w-5 text-gray-400" aria-hidden="true" ></i>
+            </RouterLink>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+</template>
---
+++ src/components/Team.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="bg-white">
+    <div class="mx-auto max-w-7xl py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
+      <div class="space-y-8 sm:space-y-12">
+        <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+          <h2 class="text-3xl font-bold tracking-tight sm:text-4xl">The Management</h2>
+          <p class="text-xl text-gray-500">Our team consists of methodologists and technologists, old hands and young talents as well as generalists and specialists. Our experts all have one thing in common: enthusiasm for IT.</p>
+        </div>
+        <ul role="list" class="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
+          <li v-for="person in people" :key="person.name">
+            <div class="space-y-4">
+              <img class="mx-auto h-800 w-450 rounded-full lg:h-24 lg:w-24 object-cover" :src="person.imageUrl" alt="" />
+              <div class="space-y-2">
+                <div class="text-xs font-medium lg:text-sm">
+                  <h3>{{ person.name }}</h3>
+                  <p class="text-gray-500">{{ person.role }}</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const people = [
+  {
+    name: 'Carlos Barragan',
+    role: 'Chief Technologist',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Carlos-Barragan-800x450.jpg',
+  },
+  {
+    name: 'Stefan Bleicher',
+    role: 'Operations Manager Business Areas',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Stefan-Bleicher-800x450.jpg',
+  },
+  {
+    name: 'Ruben Burr',
+    role: 'Director Digital Product Development',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Mitarbeiterportraits_Ruben-Burr_084_web-800x450.jpg',
+  },
+  {
+    name: 'Marco Dietrich',
+    role: 'Head of Finance & Administration',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Marco-Dietrich-800x450.jpg',
+  },
+  {
+    name: 'Rita Ehses',
+    role: 'Managing Director',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Rita-Ehses-800x450.jpg',
+  },
+  {
+    name: 'Andreas Falk',
+    role: 'Security Expert',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Andreas-Falk-800x450.jpg',
+  },
+  {
+    name: 'Jonas Grundler',
+    role: 'Director New Business Development',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Jonas-Grundler-800x450.jpg',
+  },
+  {
+    name: 'Matthias Haeussler',
+    role: 'Chief Technologist',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/2018/09/Novatec_Mitarbeiterportraits_Matthias-Haeussler_007_heller-800x450.jpg',
+  },
+  {
+    name: 'Thorsten Jakoby',
+    role: 'Director Technology Expertise',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/2018/09/Novatec_Mitarbeiterportraits_Thorsten-Jakoby_040-800x450.jpg',
+  },
+  {
+    name: 'Rolf Knoll',
+    role: 'Enterprise Architecture Management Expert',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Rolf-Knoll-800x450.jpg',
+  },
+  {
+    name: 'Lutz Malburg',
+    role: 'Agile Organization Expert',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/2018/10/Novatec_Mitarbeiterportraits_Lutz-Malburg_034-800x450.jpg',
+  },
+  {
+    name: 'Dominik Meyer',
+    role: 'Managing Director',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Mitarbeiterportraits_Dominik-Meyer_218_WEB-800x450.jpg',
+  },
+  {
+    name: 'Markus Müller',
+    role: 'Director Digital Product Development',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Markus-Mueller-800x450.jpg',
+  },
+  {
+    name: 'Georg Rekas',
+    role: 'Agile Organization Expert',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Georg-Rekas_235k-800x450.jpg',
+  },
+  {
+    name: 'Markus Scheel',
+    role: 'Head of Technical Services & Information Security Officer',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Markus-Scheel-800x450.jpg',
+  },
+  {
+    name: 'Kimia Schneider',
+    role: 'Lead Marketing',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Mitarbeiterportraits_Kimia-Faghihi_001-800x450.jpg',
+  },
+  {
+    name: 'Michael Schuchart',
+    role: 'Managing Director',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Michael-Schuchart-800x450.jpg',
+  },
+  {
+    name: 'Sabine Sonn',
+    role: 'Head of People & Culture',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Mitarbeiterportraets_Sabine-Sonn_150-800x450.jpg',
+  },
+  {
+    name: 'Stephan van de Graaf',
+    role: 'Director Technology Expertise',
+    imageUrl:
+      'https://www.novatec-gmbh.de/wp-content/uploads/Novatec_Mitarbeiterportraets-VandeGraaf-Stephan-066-800x450.webp',
+  },
+]
+</script>
