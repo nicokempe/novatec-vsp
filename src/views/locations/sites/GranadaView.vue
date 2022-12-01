4c147bbbdc9f3516b5906ce076f9ff0029a887b1
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-12-01 01:22:39 +0100

    Add the site views

---
+++ src/views/locations/sites/GranadaView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-white">
+    <div class="mx-auto max-w-2xl py-16 px-12 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
+      <LocationHead :siteName="siteNameData" :siteLocation='siteLocationData' :siteCreated='siteCreatedData' :siteEmployees="siteEmployeesData" :applicationFilter='applicationFilterData'></LocationHead>
+      <p class="mt-5">[Location content]</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import LocationHead from '@/components/LocationHead';
+
+export default {
+components: {
+  LocationHead
+},
+data() {
+  return {
+    siteNameData: 'Granada',
+    siteLocationData: 'Spain',
+    siteCreatedData: '',
+    siteEmployeesData: '51',
+    applicationFilterData: '/career/search?filter=granada',
+  }
+}
+}
+</script>
